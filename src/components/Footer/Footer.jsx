24b071481dcd1e94819a3b1e2346eefd2387c648
Ahmad <ahmad.alsayed@dci-student.org>
2025-03-05 08:33:00 +0100

    done

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -2,30 +2,36 @@ import "./Footer.scss";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faFacebook, faInstagram, faLinkedin, faGithub } from "@fortawesome/free-brands-svg-icons";
 
+
+
+
+
+
+
+
+
+
+
+
+
+
 const Footer = () => {
     return (
       <footer id="Contact">
         <p>&copy; 2024 Ahmad Portfolio. All rights reserved.</p>
         <section>
+          
           <a href="#">
-            <i className="icon-wrapper">
-              <FontAwesomeIcon icon={faFacebook} style={{ color: "#49f3f1" }} />
-            </i>
+            <FontAwesomeIcon icon={faFacebook} style={{ color: "#49f3f1", fontSize: "24px" }} />
           </a>
           <a href="#">
-            <i className="icon-wrapper">
-              <FontAwesomeIcon icon={faInstagram} style={{ color: "#49f3f1" }} />
-            </i>
+            <FontAwesomeIcon icon={faInstagram} style={{ color: "#49f3f1", fontSize: "24px" }} />
           </a>
           <a href="#">
-            <i className="icon-wrapper">
-              <FontAwesomeIcon icon={faLinkedin} style={{ color: "#49f3f1" }} />
-            </i>
+            <FontAwesomeIcon icon={faLinkedin} style={{ color: "#49f3f1", fontSize: "24px" }} />
           </a>
           <a href="#">
-            <i className="icon-wrapper">
-              <FontAwesomeIcon icon={faGithub} style={{ color: "#49f3f1" }} />
-            </i>
+            <FontAwesomeIcon icon={faGithub} style={{ color: "#49f3f1", fontSize: "24px" }} />
           </a>
         </section>
         <p>
@@ -36,45 +42,3 @@ const Footer = () => {
 };
 
 export default Footer;
-
-
-
-
-
-
-
-
-
-
-
-
-// import "./Footer.scss";
-// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-// import { faFacebook, faInstagram, faLinkedin, faGithub } from "@fortawesome/free-brands-svg-icons";
-
-// const Footer = () => {
-//     return (
-//       <footer id="Contact">
-//         <p>&copy; 2024 Ahmad Portfolio. All rights reserved.</p>
-//         <section>
-//           <a href="#">
-//             <FontAwesomeIcon icon={faFacebook} style={{ color: "#49f3f1", fontSize: "24px" }} />
-//           </a>
-//           <a href="#">
-//             <FontAwesomeIcon icon={faInstagram} style={{ color: "#49f3f1", fontSize: "24px" }} />
-//           </a>
-//           <a href="#">
-//             <FontAwesomeIcon icon={faLinkedin} style={{ color: "#49f3f1", fontSize: "24px" }} />
-//           </a>
-//           <a href="#">
-//             <FontAwesomeIcon icon={faGithub} style={{ color: "#49f3f1", fontSize: "24px" }} />
-//           </a>
-//         </section>
-//         <p>
-//           Contact me: <a href="tel:+49567890760">+49 567-890-760</a>
-//         </p>
-//       </footer>
-//     );
-// };
-
-// export default Footer;
