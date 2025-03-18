7cf32e6bea68bde7e7eed5af92aec83aa4c24065
Ahmad <ahmad.alsayed@dci-student.org>
2025-03-18 08:03:39 +0100

    removedTheNavBArShadow

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -8,7 +8,7 @@ import "./Projects.scss";
 import { MdKeyboardArrowUp } from "react-icons/md";
 import { useState } from "react";
 import { projects } from "./ProjectsData";
-
+import { BsTranslate } from "react-icons/bs";
 
 export default function Projects() {
   const [stylingDescriptionIndex, setStylingDescriptionIndex] = useState(null);
@@ -32,7 +32,7 @@ export default function Projects() {
         speed={6000} 
         autoplay={{
           delay: 0, // Auto slide every 3 seconds
-          disableOnInteraction: false,
+          disableOnInteraction: true,
           pauseOnMouseEnter:true // Keep autoplay after interaction
         }}
         breakpoints={{
@@ -49,17 +49,17 @@ export default function Projects() {
             <motion.article initial={{ opacity: 0, scale: 0.8 }} animate={{ opacity: 1, scale: 1 }} transition={{ duration: 0.5, delay: index * 0.3 }}>
               <h1 style={{ color: "white", padding: "1rem 0rem" }}>{project.title}</h1>
               <figure>
+                {/* Image with no scaling on hover */}
                 <motion.img
                   src={project.imgSrc}
                   alt={project.altText}
-                  whileHover={{ scale: 1.1 }}
-                  transition={{ duration: 0.3 }}
+                  whileHover={{ x: 0, y: 0, scale: 1 ,background:'red'}}                   transition={{ duration: 0 }}  // No transition for hover stop
                 />
               </figure>
 
               <motion.button
                 onClick={() => handleOpenTHeDescription(index)}
-                whileHover={{ scale: 1.1 }}
+                whileHover={{ scale: 1 }}  // No scaling on hover
                 whileTap={{ scale: 0.9 }}
                 transition={{ duration: 0.2 }}
               >
